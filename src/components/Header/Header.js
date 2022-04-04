d58705518e8156a61340f8f54ea8e756c2b448c8
nusrat78 <njahan5061@gmail.com>
2022-04-05 01:39:17 +0600

    navbar setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='text-decoration-line: none'>
+            <nav className='space-x-12 font-bold text-2xl'>
+                <a href="/">Home</a>
+                <a href="/reviews
+                ">Reviews</a>
+                <a href="/dashboard
+                ">Dashboard</a>
+                <a href="/blogs
+                ">Blogs</a>
+                <a href="/about
+                ">About</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
